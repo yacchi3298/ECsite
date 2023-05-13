c8fc4deed4e0195c783154c2f4e015221024e628
yacchi3298 <yacchi1854@icloud.com>
2023-05-13 14:00:33 +0900

    リクエストスペック追加name,description

--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -1,7 +1,41 @@
 require 'rails_helper'
+require 'spree/testing_support/factories'
 
 RSpec.describe "Potepan::Products", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+
+  describe "#show" do
+
+    let!(:product) { create(:product) }
+    let!(:image) { create(:image) }
+    before do
+      get potepan_product_path(product.id)
+      product.images << [image]
+      # 画像URLの取得が上手くいかない問題への対応
+      # https://mng-camp.potepan.com/curriculums/document-for-final-task-2#notes-of-image-test
+      ActiveStorage::Current.host = request.base_url
+      product.reload
+      image.reload
+    end
+
+    it "詳細ページを正常に取得できること" do
+      expect(response).to have_http_status(200)
+    end
+
+    it "商品名を正常に取得できること" do
+      expect(response.body).to include product.name
+    end
+
+    it "商品の値段を正常に取得できること" do
+      expect(response.body).to include product.price
+    end
+
+    it "商品名を正常に取得できること" do
+      expect(response.body).to include product.description
+    end
+
+    it "商品画像を正常に取得できること" do
+      expect(response.body).to include product.images
+    end
   end
+
 end
