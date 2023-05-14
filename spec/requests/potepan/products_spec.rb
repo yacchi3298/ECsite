1948114d7f016127c50fb28e39c8fbfcf4c30b60
yacchi3298 <yacchi1854@icloud.com>
2023-05-14 21:28:29 +0900

    パーシャル切り出し、タイムゾーン修正、タイトルタグ修正

--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "Potepan::Products", type: :request do
     let!(:product) { create(:product) }
     let!(:image) { create(:image) }
     before do
-      product.images << [image]
+      product.images << image
       get potepan_product_path(product.id)
       # 画像URLの取得が上手くいかない問題への対応
       # https://mng-camp.potepan.com/curriculums/document-for-final-task-2#notes-of-image-test
--- spec/systems/products_systems_spec.rb
+++
@@ -1,9 +0,0 @@
-# require 'rails_helper'
-# require 'spree/testing_support/factories'
-
-# RSpec.describe "Potepan::Products Systems", type: :system do
-#   before do
-#     driven_by(:rack_test)
-#   end
-
-# end
