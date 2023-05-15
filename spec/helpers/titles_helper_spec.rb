42e4cf3186fdf4bce8a125ca11ceb883f2f486e3
yacchi3298 <yacchi1854@icloud.com>
2023-05-15 15:43:37 +0900

    helperspec修正

--- spec/helpers/titles_helper_spec.rb
+++ spec/helpers/titles_helper_spec.rb
@@ -1,21 +1,18 @@
 require 'rails_helper'
 require 'spree/testing_support/factories'
-include ApplicationHelper
-
 
 RSpec.describe ApplicationHelper, type: :helper do
-
   describe "#full_title" do
     context "productに値が入っている場合" do
-      let(:product) { create(:product,name:"SOLIDUS GIRLY") }
+      let(:product) { create(:product, name: "SOLIDUS GIRLY") }
       it 'show用タイトルが正常に表示される' do
-        expect(helper.full_title(product.name)).to eq"#{product.name} - #{BASE_TITLE}"
+        expect(helper.full_title(product.name)).to eq "#{product.name} - BIGBAG Store"
       end
     end
 
     context "productに値が入っていない場合" do
       it "index用タイトルが正常に表示される" do
-        expect(helper.full_title(nil)).to eq BASE_TITLE
+        expect(helper.full_title(nil)).to eq "BIGBAG Store"
       end
     end
   end
