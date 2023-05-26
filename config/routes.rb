a76a62799b300573f5bc3e57075f6bd8ec7bae87
yacchi3298 <yacchi1854@icloud.com>
2023-05-27 08:31:49 +0900

    html修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  namespace :potepan do
-    get 'categories/show'
-  end
 mount SolidusPaypalCommercePlatform::Engine, at: '/solidus_paypal_commerce_platform'
   # This line mounts Solidus's routes at the root of your application.
   # This means, any requests to URLs such as /products, will go to Spree::ProductsController.
