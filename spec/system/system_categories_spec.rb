76411757a024c9045d36ee0bafa6d9e1e9472a52
yacchi3298 <yacchi1854@icloud.com>
2023-05-27 00:15:37 +0900

    システムスペック商品数のテスト追加

--- spec/system/system_categories_spec.rb
+++ spec/system/system_categories_spec.rb
@@ -1,18 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Categoriesページ", type: :system do
-  describe "カテゴリー表示" do
-    let(:taxonomy) { create(:taxonomy, name: "Category") }
-    let!(:taxon) { create(:taxon, name: "shirts", parent: taxonomy.root) }
-    let!(:taxon2) { create(:taxon, name: "hoodie", parent: taxonomy.root) }
-    let(:product) { create(:product, name: "ruby polo", price: 15, taxons: [taxon]) }
-    let(:image) { create(:image) }
-    before do
-      driven_by(:rack_test)
-      product.images << image
-      visit potepan_category_path(taxon.id)
-    end
+  let(:taxonomy) { create(:taxonomy, name: "Category") }
+  let!(:taxon) { create(:taxon, name: "shirts", parent: taxonomy.root) }
+  let!(:taxon2) { create(:taxon, name: "hoodie", parent: taxonomy.root) }
+  let(:product) { create(:product, name: "ruby polo", price: 15, taxons: [taxon]) }
+  let(:product2) { create(:product, name: "solidus girly", price: 12, taxons: [taxon2]) }
+  let(:image) { create(:image) }
+  before do
+    driven_by(:rack_test)
+    product.images << image
+    visit potepan_category_path(taxon.id)
+  end
 
+  describe "カテゴリーページの表示について" do
     it "カテゴリーページのパスが正しいこと" do
       expect(page).to have_current_path potepan_category_path(taxon.id)
     end
@@ -24,16 +25,25 @@ RSpec.describe "Categoriesページ", type: :system do
     it "商品が正常に表示されること" do
       expect(page).to have_content "ruby polo"
       expect(page).to have_content product.display_price.to_s
+      expect(page).not_to have_content "solidus girly"
+      expect(page).not_to have_content product2.display_price.to_s
     end
 
-    it "カテゴリーが正常に表示されること" do
+    it "サイドバーのカテゴリーが正常に表示されること" do
       within ".side-nav" do
         click_on 'Category'
         expect(page).to have_content "shirts"
         expect(page).to have_content "Category"
+        expect(page).to have_content taxon.all_products.count
       end
     end
 
+    it "ページに表示される商品数がカテゴリーリストの商品数と一致すること" do
+      expect(page.all('.productBox').count).to eq taxon.all_products.count
+    end
+  end
+
+  describe "リンクの動作について" do
     it "header_light_section HOMEリンククリックでトップページに正常に遷移できること" do
       within ".lightSection_home" do
         click_on 'HOME'
--- spec/system/system_products_spec.rb
+++ spec/system/system_products_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "SystemProducts", type: :system do
-  describe "products表示" do
-    let!(:taxon) { create(:taxon) }
-    let(:product) { create(:product, name: "ruby polo", price: 15, taxons: [taxon]) }
-    let(:image) { create(:image) }
-    before do
-      driven_by(:rack_test)
-      product.images << image
-      visit potepan_product_path(product.id)
-    end
+  let!(:taxon) { create(:taxon) }
+  let(:product) { create(:product, name: "ruby polo", price: 15, taxons: [taxon]) }
+  let(:image) { create(:image) }
+  before do
+    driven_by(:rack_test)
+    product.images << image
+    visit potepan_product_path(product.id)
+  end
 
+  describe "productsページの表示について" do
     it "商品詳細ページのパスが正しいこと" do
       expect(page).to have_current_path potepan_product_path(product.id)
     end
 
-    it "商品詳細ページのタイトルを確認" do
+    it "商品詳細ページのタイトルが正しいこと" do
       expect(page).to have_title "ruby polo - BIGBAG Store"
     end
 
@@ -23,7 +23,9 @@ RSpec.describe "SystemProducts", type: :system do
       expect(page).to have_content "ruby polo"
       expect(page).to have_content product.display_price.to_s
     end
+  end
 
+  describe "リンクの動作について" do
     it "一覧ページへ戻るリンククリックでカテゴリーページに遷移できること" do
       click_on '一覧ページへ戻る'
       expect(page).to have_current_path potepan_category_path(taxon.id)
